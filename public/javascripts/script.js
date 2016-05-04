2bc64b763f82da58809750d9e4f05a6ef49e119b
zolon wilkins <zolonsk8@gmail.com>
2016-05-04 14:30:44 -0500

    initial commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,71 @@
+$(function(){
+
+  $('#form').mouseover(function(){
+   $(this).attr("placeholder", "tell me your name");
+    $('#form').mouseleave(function(){
+     $(this).attr("placeholder", "ask me about the weather");
+    })
+   })
+ $('#form').keypress(function(e){
+   var q = $('#form').val()
+   
+   if(e.which == 13) {
+    $('#r').html('')
+    $('#pic').html('')
+    $.ajax({
+     url: "https://api.wit.ai/message" ,
+     data: {
+      'q' : q,
+      'access_token' : '5OCANSDN37ESNPMEARLQ6KDYNGTM355X'
+     },
+     dataType : 'jsonp',
+     method: "POST",
+     success: function(response) {
+
+      var entity = response.outcomes[0].entities
+
+       if ('contact' in entity) {
+       var contactVal = response.outcomes[0].entities.contact[0].value
+
+       $('#r').append('Hello ' + contactVal + '! I am updog')
+      } else if ('location' in entity){
+
+       var city = response.outcomes[0].entities.location[0].value
+        $.ajax({
+           method: "GET",
+           url: "https://maps.googleapis.com/maps/api/geocode/json?address="+ city+"&key=AIzaSyAHAR1gTNA7hxRl3zOMpWZswWJuAc0Idi4"
+          }).done(function(response){
+           console.log(response)
+
+           var lat = response.results[0].geometry.location.lat
+           var lng = response.results[0].geometry.location.lng
+           var city = response.results[0].formatted_address
+
+           $.ajax({
+            method: "GET",
+            dataType: 'jsonp',
+            url: "https://api.forecast.io/forecast/393009429e2d58513731179ff376b6ce/"+lat+"," +lng
+           }).done(function(response){
+            var temp = response.currently.apparentTemperature
+             $('#r').append("It's " + temp + "&deg; in " + city)
+          })
+          })
+
+      } else if ('sup' in entity) {
+       var responses = ['just hangin out', 'just chillin', 'not much', 'dank memes', 'my chances of being funded']
+       var reply = responses[Math.floor(Math.random()*responses.length)];
+       $('#r').append(reply)
+
+      } else if ('joke' in entity) {
+       $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+       $('#pic').append('<img class="img-fluid pull-xs-center heboot" src="http://i.imgur.com/zgOt8dH.jpg" /> ')
+      }
+      else {
+       $('#r').append('whooops')
+      }
+     }
+    });
+   }
+  }
+ )
+ })
