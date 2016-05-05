ec560934ec1eb6bd0ddf200695eee952c45ad4bc
zolon wilkins <zolonsk8@gmail.com>
2016-05-05 14:25:20 -0500

    design sweep and jquery fun

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,27 @@
 $(function(){
 
-  $('#form').mouseover(function(){
-    $(this).attr("placeholder", "tell me your name");
-    $('#form').mouseleave(function(){
-     $(this).attr("placeholder", "ask me about the weather");
-    })
-   })
+$(document).ready(function() {
+ $("#form").animate({ marginTop: 0, opacity: 1 }, 1000, function(){
+  $('.helpers').animate({opacity: 1},1500)
+  })
+ })
+
+ $('#meme-btn').on('click', function(){
+  $('#form').val('meme me')
+ })
+ $('#drake').on('click', function(){
+  $('#form').val('play drake')
+ })
+ $('#weather').on('click', function(){
+  $('#form').val('austin weather')
+ })
+
 
  $('#form').keypress(function(e){
    var q = $('#form').val()
    if(e.which == 13) {
+    $('#pink').animate({ height: '50vh'},800)
+
     $('#r').html('')
     $('#pic').html('')
     $('#meme').html('')
@@ -40,6 +52,17 @@ $(function(){
           $('#r').append('<img src="' + src+ '" class="heboot"/ >')
          }
         })
+     } else if ('play' in entity && 'artist' in entity) {
+      var artist = response.outcomes[0].entities.artist[0].value
+      $.ajax({
+       method: "GET",
+       url:"https://api.spotify.com/v1/search?q="+ artist +"&type=artist"
+      }).done(function(res){
+       var uri = res.artists.items[0].uri
+       console.log(res.artists.items[0].uri)
+       $('#r').css("margin-top", "1%")
+       $('#pic').append('<iframe src="https://embed.spotify.com/?uri=' + uri+ '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>')
+      })
      } else if ('contact' in entity) {
        var contactVal = response.outcomes[0].entities.contact[0].value
        $('#r').append('Hello ' + contactVal + '! I am updog')
@@ -71,16 +94,6 @@ $(function(){
           } else if ('joke' in entity) {
            $("html, body").animate({ scrollTop: $(document).height() }, 1000);
            $('#pic').append('<img class="img-fluid pull-xs-center heboot" src="http://i.imgur.com/zgOt8dH.jpg" /> ')
-          } else if ('play' in entity && 'artist' in entity) {
-           var artist = response.outcomes[0].entities.artist[0].value
-           $.ajax({
-            method: "GET",
-            url:"https://api.spotify.com/v1/search?q="+ artist +"&type=artist"
-           }).done(function(res){
-            var uri = res.artists.items[0].uri
-            console.log(res.artists.items[0].uri)
-            $('#pic').append('<iframe src="https://embed.spotify.com/?uri=' + uri+ '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>')
-           })
           } else if ('song' in entity) {
            $('#r').append('I love These')
            $('#pic').append('<iframe src="https://embed.spotify.com/?uri=spotify:user:gorgonzolon:playlist:4T6FSZva3M8nZgHHNeRFi3" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>')
