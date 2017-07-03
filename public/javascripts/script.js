1b34a6ff37728c04c35c61e9ef3993546e9e6add
Zolon Wilkins <zolonsk8@gmail.com>
2017-07-02 22:22:40 -0500

    sweep

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,35 +1,40 @@
 $(function(){
 
-$(document).ready(function() {
-  $("#form").animate({ marginTop: 0, opacity: 1 }, 1000, function(){
-  var helpers = ['try asking for the "weather in austin"', 'try "hello"', 'try "its dat boi"', 'try "play mac demarco"']
-  var helper = helpers[Math.floor(Math.random()*helpers.length)]
-  $('.helpers').append('<small class="text-muted">'+ helper + '</small>')
-  $('.helpers').animate({opacity: 1},1500)
-  })
- })
+  $(document).ready(function() {
+    $("#form").animate({ marginTop: 0, opacity: 1 }, 1000, function(){
+      var helpers = ['try asking for the "weather in austin"', 'try "hello"', 'try "its dat boi"', 'try "play mac demarco"']
+      var helper = helpers[Math.floor(Math.random()*helpers.length)]
+      $('.helpers').append('<small class="text-muted">'+ helper + '</small>')
+      $('.helpers').animate({opacity: 1},1500)
+    })
+   })
 
- $('#form').keypress(function(e){
-  $('body').css('overflow-y','auto')
-  $('.wrapper').css('overflow-y','auto')
 
-   var q = $('#form').val()
-   if(e.which == 13) {
-    $('#pink').animate({ height: '50vh'},800)
-
+  var clear = function() {
     $('#r').html('')
     $('#pic').html('')
     $('#meme').html('')
     $('#form').blur();
     $('#weather').html('')
+  }
+
+ $('#form').keypress(function(e){
+
+  $('body').css('overflow-y','auto')
+  $('.wrapper').css('overflow-y','auto')
+  var q = $('#form').val()
+
+  if(e.which == 13) {
+    $('#pink').animate({ height: '50vh'},800)
+    clear()
 
     $.ajax({
-     url: "/wit/" + q ,
-     method: "GET",
-     success: function(response) {
-       console.log(response)
-       var entity = response.entities
-       if ('meme' in entity) {
+      url: "/wit/" + q ,
+      method: "GET",
+      success: function(response) {
+        console.log(response)
+        var entity = response.entities
+        if ('meme' in entity) {
           $.ajax({
             method: "GET",
             url: "https://www.reddit.com/r/me_irl.json",
@@ -44,43 +49,30 @@ $(document).ready(function() {
         })
      } else if ('play' in entity && 'artist' in entity) {
         var artist = response.entities.artist[0].value
-        $.ajax({
-          method: "GET",
-          url:"https://api.spotify.com/v1/search?q="+ artist +"&type=artist"
-        }).done(function(res){
-          var uri = res.artists.items[0].uri
+        $.ajax({ method: "GET", url:"/spotify/" + artist }).done(function(res){
+          var uri = res.uri
           $('#r').css("margin-top", "-20px")
           $('#pic').append('<iframe src="https://embed.spotify.com/?uri=' + uri+ '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>')
         })
       } else if ('datboi' in entity) {
-        $('#r').append('oh shit whaddup')
-
+          $('#r').append('oh shit whaddup')
       } else if ('updog' in entity) {
-        $('#r').append('not much, wbu?')
-
+          $('#r').append('not much, wbu?')
       } else if ('your' in entity) {
-        $('#r').append('I am updog')
-
+          $('#r').append('I am updog')
       } else if ('greeting' in entity) {
-        $('#r').append('yes this is updog')
-
+          $('#r').append('yes this is updog')
       } else if ('bork' in entity) {
-        $('#r').append('jesus you did me the really big frighten')
-
+          $('#r').append('jesus you did me the really big frighten')
       } else if ('tomorrow_' in entity && 'location' in entity ) {
-        var city = response.entities.location[0].value
-        $.ajax({
-          method: "GET",
-          url: "/geocode/" + city
-        }).done(function(response){
-          var lat = response.results[0].geometry.location.lat
-          var lng = response.results[0].geometry.location.lng
-          var city = response.results[0].formatted_address
+          var city = response.entities.location[0].value
 
-          $.ajax({
-            method: "GET",
-            url: '/forecast/' +lat+ ',' + lng,
-          }).done(function(response){
+          $.ajax({ method: "GET", url: "/geocode/" + city }).done(function(response){
+            var lat = response.results[0].geometry.location.lat
+            var lng = response.results[0].geometry.location.lng
+            var city = response.results[0].formatted_address
+
+          $.ajax({ method: "GET", url: '/forecast/' +lat+ ',' + lng }).done(function(response){
               var summary = response.daily.data[1].summary
               var max = response.daily.data[1].apparentTemperatureMax
               var min = response.daily.data[1].apparentTemperatureMin
@@ -88,32 +80,24 @@ $(document).ready(function() {
             })
          })
        } else if ('location' in entity){
-         var city = response.entities.location[0].value
-          $.ajax({
-            method: "GET",
-            url: "/geocode/" + city
-            }).done(function(response){
+          var city = response.entities.location[0].value
+          $.ajax({ method: "GET", url: "/geocode/" + city }).done(function(response){
               var lat = response.results[0].geometry.location.lat
               var lng = response.results[0].geometry.location.lng
               var city = response.results[0].formatted_address
 
-             $.ajax({
-               method: "GET",
-               url: '/forecast/' +lat+ ',' + lng,
-              }).done(function(response){
+              $.ajax({ method: "GET", url: '/forecast/' +lat+ ',' + lng, }).done(function(response){
                 var temp = response.currently.apparentTemperature
                 $('#r').append("It's " + temp + "&deg; in " + city)
               })
-            })
+          })
         } else if ('sup' in entity) {
            var responses = ['just hangin out', 'just chillin', 'not much', 'eh not too much', 'not a lot']
            var reply = responses[Math.floor(Math.random()*responses.length)];
            $('#r').append(reply)
-
         } else if ('joke' in entity) {
-         $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-         $('#pic').append('<img class="img-fluid pull-xs-center heboot" src="http://i.imgur.com/zgOt8dH.jpg" /> ')
-
+          $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+          $('#pic').append('<img class="img-fluid pull-xs-center heboot" src="http://i.imgur.com/zgOt8dH.jpg" /> ')
         } else if ('song' in entity) {
           $('#r').append('I love These')
           $('#pic').append('<iframe src="https://embed.spotify.com/?uri=spotify:user:gorgonzolon:playlist:4T6FSZva3M8nZgHHNeRFi3" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>')
@@ -124,6 +108,8 @@ $(document).ready(function() {
        }
       );
      }
-    }
-   )
+
+    })
+
+
  })
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var Client = require('node-rest-client').Client;
 var client = new Client();
-
+var pry = require('pryjs')
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -11,12 +12,8 @@ router.get('/', function(req, res, next) {
 
 router.get('/wit/:q', function(req,res,next) {
   var q = req.params.q
-  var args = {
-    headers: {
-      "Authorization": " Bearer " + process.env.UPDOG_WIT
-    }
-  }
-  client.post(`https://api.wit.ai/message?q=${q}`, args, function(data, response) {
+  var params = {  headers: { "Authorization": `Bearer ${process.env.UPDOG_WIT}` } }
+  client.post(`https://api.wit.ai/message?q=${q}`, params, function(data, response) {
     res.json(data)
   })
 })
@@ -31,6 +28,7 @@ router.get('/forecast/:lat,:lng', function(req,res,next){
   })
 })
 
+
 router.get('/geocode/:city', function(req,res,next) {
   var city = req.params.city
   client.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GOOGLE_API}`, function(data, response){
@@ -39,6 +37,27 @@ router.get('/geocode/:city', function(req,res,next) {
   })
 })
 
+
+router.get('/spotify/:q', function(req,res,next) {
+  var q = req.params.q
+  var b = new Buffer(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET);
+  var credentials = b.toString('base64');
+  var headers = {
+    headers: {
+      "Authorization": `Basic ${credentials}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+  }
+  client.post(`https://accounts.spotify.com/api/token?grant_type=client_credentials`, headers, function(data, response) {
+    if(!data.error) {
+      var token = data.access_token
+      client.get(`https://api.spotify.com/v1/search?q=${q}&type=artist&access_token=${token}`, function(data, response){
+        res.json({uri: data.artists.items[0].uri})
+      })
+    }
+  })
+
+})
 
 
 module.exports = router;
